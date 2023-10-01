854e956c1eb9608feedf811e3ae087f30c28e6ce
chessai <chessai1996@gmail.com>
2023-09-30 22:59:42 -0500

    delete redundant comment in MelterPatch

--- src/main/java/UniversalCardFix/patches/cards/blue/MelterPatch.java
+++ src/main/java/UniversalCardFix/patches/cards/blue/MelterPatch.java
@@ -11,15 +11,4 @@ public class MelterPatch {
             __instance.baseDamage = 11;
         }
     }
-/*
-    @SpirePatch(clz = Melter.class, method = "upgrade")
-    public static class Upgrade {
-        public static void Replace(Melter __instance) {
-            if (!__instance.upgraded) {
-                Utils.cardUpgradeName(__instance);
-                Utils.cardUpgradeDamage(__instance, 1);
-            }
-        }
-    }
-*/
 }
